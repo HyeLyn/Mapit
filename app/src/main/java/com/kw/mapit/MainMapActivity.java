7c4c9dec3b859388a8d5040f86db456270d3b2f4
Sunha03 <ahnus123@gmail.com>
2018-03-26 03:33:41 +0900

    Add naver map(main)

--- app/src/main/java/com/kw/mapit/MainMapActivity.java
+++ app/src/main/java/com/kw/mapit/MainMapActivity.java
@@ -3,11 +3,26 @@ package com.kw.mapit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainMapActivity extends AppCompatActivity {
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapView;
+
+public class MainMapActivity extends NMapActivity {
+
+    private NMapView mMapView;          //지도 화면 View
+    private final String CLIENT_ID = "YOUR_CLIENT_ID";      //CLIENT_ID 값
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_map);
+
+        mMapView = new NMapView(this);
+        setContentView(mMapView);
+        mMapView.setClientId(CLIENT_ID);    //CLIENT_ID 값 설정
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.requestFocus();
     }
 }
